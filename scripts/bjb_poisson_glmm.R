2e0edb40dcea6e2de459988fe374fec8509d2d34
Brendan J Barrett <bjb5009@gmail.com>
2026-07-17 12:49:48 +0200

    plot preds in brms

--- scripts/bjb_poisson_glmm.R
+++ scripts/bjb_poisson_glmm.R
@@ -43,6 +43,36 @@ model <- brm(
   data = ds2,
   family = poisson(link = "log"),
   chains = 4, 
-  iter = 2000
+  iter = 2000,
+  backend = "cmdstan"
 )
 
+summary(model)
+plot(model)
+# Basic conditional effects plot
+conditional_effects(model)
+
+# Plotting a specific interaction with raw data points overlayed
+plot(conditional_effects(fit_model, effects = "tool_use"), points = TRUE)
+library(posterior)
+
+draws <- as_draws_df(model)
+
+# View the first few rows and columns
+summary(draws)
+plot(density(exp(draws$b_Intercept)))
+plot(density(exp(draws$b_Intercept + draws$b_tool_use)) , add=TRUE, col="green4")
+
+library(rethinking)
+dens(exp(draws$b_Intercept) , xlim=c(0,20) , ylim = c(-.1,.8))
+dens(exp(draws$b_Intercept + draws$b_tool_use) , add=TRUE , col="salmon2")
+##lets plot predictions, need to get on scale of preds
+flop <-ds2$total_process_duration_m[ds2$tool_use==0] 
+flip <-ds2$total_process_duration_m[ds2$tool_use==1]
+points(flop, rep(0 , length(flop)))
+points(flip, rep(-.1 , length(flip)) , col="salmon2")
+
+plot(density(exp(draws$b_Intercept + draws$b_tool_use)) , add=TRUE, col="green4")
+
+
+#### add batch later
